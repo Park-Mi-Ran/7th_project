1866ae550182f560a3a31bd4baf871737b5d50e2
Park-Mi-Ran <gpfk6636@naver.com>
2021-06-04 23:39:42 +0900

    7주차 과제 정답입니당

--- crud/crud/settings.py
+++ crud/crud/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'crud.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['crud/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,6 +120,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'signup', 'static')
+]
+# static 파일들이 현재 어디에 있는지를 쓰는 곳
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# static 파일들이 어디로 모일 것인지를 쓰는 곳
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- crud/signup/views.py
+++ crud/signup/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render, get_object_or_404
+from .models import Signup
+from django.utils import timezone
 # Create your views here.
+
+def home(request):
+    signup = Signup.objects.all()
+    return render(request,'home.html',{'signup':signup})
+
+def detail(request, id):
+    detail = get_object_or_404(Signup, pk = id)
+    return render(request, 'detail.html', {'detail':detail})
+
+def new(request):
+    return render(request, 'new.html')
+
+def create(request):
+    new_member = Signup()
+    new_member.name = request.POST['name']
+    new_member.age = request.POST['age']
+    new_member.email = request.POST['email']
+    new_member.introduce = request.POST['introduce']
+    new_member.pub_date = timezone.now()
+    new_member.save()
+    return redirect('detail', new_member.id)
+
+def edit(request,id):
+    edit_member = Signup.objects.get(id=id)
+    return render(request,'edit.html', {'member':edit_member})
+
+def update(request,id):
+    update_member = Signup.objects.get(id=id)
+    update_member.name = request.POST['name']
+    update_member.age = request.POST['age']
+    update_member.email = request.POST['email']
+    update_member.introduce = request.POST['introduce']
+    update_member.pub_date = timezone.now()
+    update_member.save()
+    return redirect('detail', update_member.id)
+
+def delete(request,id):
+    delete_member = Signup.objects.get(id=id)
+    delete_member.delete()
+    return redirect('home')
